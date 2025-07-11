a2d73d5f78e6ce3154091039e95515009f2993db
Ali Raza <alirazakhalid1129@gmail.com>
2025-07-12 02:22:21 +0500

    feat: theme-toggle light/dark

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function Home() {
   return (
     <div className="text-3xl flex flex-col items-center justify-center min-h-screen">
       Ali
+      <ThemeToggle />
     </div>
   );
 }
