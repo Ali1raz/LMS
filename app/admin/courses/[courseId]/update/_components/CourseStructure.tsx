ac57ba99f35f6054bfc3748bc3e6445db0f7b2e0
Ali Raza <alirazakhalid1129@gmail.com>
2025-07-18 21:14:51 +0500

    implement dnd with DndContext and SortableContext

--- app/admin/courses/[courseId]/update/_components/CourseStructure.tsx
+++ app/admin/courses/[courseId]/update/_components/CourseStructure.tsx
@@ -1,7 +1,92 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  rectIntersection,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import React, { useState } from "react";
+import { CSS } from "@dnd-kit/utilities";
+
 export default function CourseStructure() {
+  const [items, setItems] = useState([1, 2, 3]);
+
+  function SortableItem(props) {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+      useSortable({ id: props.id });
+
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+    return (
+      <div
+        className="cursor-grab"
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+      >
+        {props.id}
+      </div>
+    );
+  }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
   return (
     <>
-      <h1>Course Structure</h1>
+      <DndContext
+        collisionDetection={rectIntersection}
+        onDragEnd={handleDragEnd}
+        sensors={sensors}
+      >
+        <Card>
+          <CardHeader className="flex flex-row border-border border-b items-center justify-between">
+            <CardTitle>Chapters</CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <SortableContext
+              strategy={verticalListSortingStrategy}
+              items={items}
+            >
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </CardContent>
+        </Card>
+      </DndContext>
     </>
   );
 }
