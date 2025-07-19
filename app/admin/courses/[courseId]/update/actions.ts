5d24da368c1107f0fbba841349b2819c5e2038c0
Ali Raza <alirazakhalid1129@gmail.com>
2025-07-19 20:55:48 +0500

    feat: implement arcjet fixedWindow on admin UpdateCourse()

--- app/admin/courses/[courseId]/update/actions.ts
+++ app/admin/courses/[courseId]/update/actions.ts
@@ -4,6 +4,23 @@ import { requireAdmin } from "@/app/data/admin/require-admin";
 import { prisma } from "@/lib/prisma";
 import { ApiResponse } from "@/lib/types";
 import { CourseSchema, CourseSchemaType } from "@/lib/zod-schemas";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
 
 export async function UpdateCourse(
   data: CourseSchemaType,
@@ -12,6 +29,25 @@ export async function UpdateCourse(
   const session = await requireAdmin();
 
   try {
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: session.user.id,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been blocked due to rate limiting.",
+        };
+      }
+
+      return {
+        status: "error",
+        message: "You are a bot, if false contact our support.",
+      };
+    }
+
     const res = CourseSchema.safeParse(data);
 
     if (!res.success) {
